4e3ab4296368ba6ccd89b66a54f6d795501130e8
s8189882 <s8189882@gmail.com>
2022-10-15 09:29:01 +0530

    Added event listeners

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,6 +1,26 @@
-const salary = document.querySelector('#salary');
-const salaryOutput = document.querySelector('.salary-output');
-salaryOutput.textContent = salary.value;
-salary.addEventListener('input', function() {
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (error) {
+            textError.textContent = error;
+        }
+    });
+
+    const salary = document.querySelector('#salary');
+    const salaryOutput = document.querySelector('.salary-output');
     salaryOutput.textContent = salary.value;
+    salary.addEventListener('input', function() {
+        salaryOutput.textContent = salary.value;
+    });
 });
+
+
+
