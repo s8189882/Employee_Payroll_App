c823a76dcb6c24a225833f0c511388b3ddc6f550
s8189882 <s8189882@gmail.com>
2022-10-18 16:55:23 +0530

    Saving employee payroll object to local stoarage

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -25,9 +25,21 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const save = () => {
     try {
             let employeePayrollData = createEmloyeePayroll();
+            createAndUpdateStorage(employeePayrollData);
     } catch (error) {
         return;
     }
+}
+
+function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if (employeePayrollList != undefined) {
+        employeePayrollList.push(employeePayrollData);
+    } else {
+        employeePayrollList = [employeePayrollData]
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
 
 const createEmloyeePayroll = () => {
