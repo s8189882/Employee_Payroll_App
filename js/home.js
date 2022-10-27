217d0f9ea17199794990476b186f2d97b26de37c
s8189882 <s8189882@gmail.com>
2022-10-27 16:55:01 +0530

    Display employee details using template literals in tabular format

---
+++ js/home.js
@@ -0,0 +1,52 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    createInnerHtml();
+});
+
+const createInnerHtml = () => {
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
+    const innerHtml = `${headerHtml}
+        <tr>
+            <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -10.png"></td>
+            <td>Vashisht Sethi</td>
+            <td>Male</td>
+            <td><div class="dept-label">HR</div>
+            <div class="dept-label">Finance</div></td>
+            <td>30000000</td>
+            <td>1 October 2022</td>
+            <td>
+                <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                <img id="1" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
+            </td>
+        </tr>
+    `;
+    document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+
+/* const createInnerHtml = () => {
+    const innerHtml = `
+    <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Department</th>
+            <th>Salary</th>
+            <th>Start Date</th>
+            <th>Actions</th>
+        </tr>
+        <tr>
+            <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -10.png"></td>
+            <td>Vashisht Sethi</td>
+            <td>Male</td>
+            <td><div class="dept-label">HR</div>
+            <div class="dept-label">Finance</div></td>
+            <td>30000000</td>
+            <td>1 October 2022</td>
+            <td>
+                <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                <img id="1" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
+            </td>
+        </tr>
+    `;
+    document.querySelector('#table-display').innerHTML = innerHtml;
+} */
